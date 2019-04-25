587528091216da13c977a5508174f40b99fa7bd2
stphnmttldrf <mail@stephan-mitteldorf.com>
2019-04-25 15:38:51 +0200

    Cards
    Einführung der Shortcodes card und cardlayout
    
    Cardlayout ist fertig, card muss noch weiter ausgebaut werden.

--- shortcodes_cards.php
+++ shortcodes_cards.php
@@ -1,1 +1,59 @@
 <?php
+
+
+add_shortcode( 'card', 'card_shortcode' );
+add_shortcode( 'cardlayout', 'cardlayout_shortcode' );
+
+function card_shortcode( $atts , $content = null ) {
+
+    $output = '';
+
+    $atts = shortcode_atts(
+        array(
+            'header' => '',
+            'extra' => '',
+            'style' => '',
+            'title' => '',
+            'text' => '',
+            'button' => '',
+            'button-class' => ''
+        ), $atts);
+
+
+    $output .= '<div class="card '.$atts['extra'].'">';
+
+    $output .= do_shortcode($content);
+
+    $output .= '</div>';
+
+    return $output;
+}
+
+
+function cardlayout_shortcode( $atts, $content = null ) {
+
+    $output = '';
+
+    $atts = shortcode_atts(
+        array(
+            'type' => 'group'
+        ), $atts);
+
+
+    switch ($atts['type']) {
+        case 'group': $cardtype = 'card-group'; break;
+        case 'deck': $cardtype = 'card-deck'; break;
+        case 'columns': $cardtype = 'card-columns'; break;
+        default: $cardtype = 'card-group'; break;
+    }
+
+
+    $output .= '<div class="'.$cardtype.'">';
+
+    $output .= do_shortcode($content);
+
+    $output .= '</div>';
+
+    return $output;
+
+}
